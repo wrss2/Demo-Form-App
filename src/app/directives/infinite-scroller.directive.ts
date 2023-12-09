19fe035f7311eb8e4100574659c87ca1587f9858
Maciej <wrsswrss@gmail.com>
2023-12-10 00:45:50 +0100

    Add InfiniteScroller

---
+++ src/app/directives/infinite-scroller.directive.ts
@@ -0,0 +1,40 @@
+import { Directive, AfterViewInit, ElementRef, Input } from '@angular/core';
+import {BehaviorSubject, filter, fromEvent, map} from "rxjs";
+
+@Directive({
+  selector: '[InfiniteScroller]'
+})
+export class InfiniteScrollerDirective implements AfterViewInit {
+
+  @Input() data$:BehaviorSubject<any> = new BehaviorSubject<any>([]);
+  @Input() loading$:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  @Input() scrollContainer:any;
+
+
+  constructor(
+    private element: ElementRef
+  ) { }
+
+  ngAfterViewInit() {
+    fromEvent(this.element.nativeElement, 'scroll')
+      .pipe(
+        map(() => this.element.nativeElement),
+        filter(() => this.isScrolledToBottom()),
+      ).subscribe(()=>{
+        this.loadData()
+    })
+  }
+  isScrolledToBottom(): boolean {
+    const element = this.element.nativeElement;
+    let scrollBottom = element.scrollTop + Math.max(element.clientHeight, element.offsetHeight)
+    if(scrollBottom  >= element.scrollHeight && !this.loading$.value) {
+      return true
+    }
+    return false
+  }
+  loadData(){
+    this.loading$.next(true)
+    this.data$.next([])
+  }
+
+}
