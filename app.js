34564fc457d806cfbe9134941961ef412d1df893
Maciej <wrsswrss@gmail.com>
2023-12-18 16:51:56 +0100

    add electron

---
+++ app.js
@@ -0,0 +1,41 @@
+const {app, BrowserWindow} = require('electron')
+    const url = require("url");
+    const path = require("path");
+
+    let mainWindow
+
+    function createWindow () {
+      mainWindow = new BrowserWindow({
+        width: 1350,
+        height: 800,
+        webPreferences: {
+          nodeIntegration: true,
+		  devTools: false
+		  
+        }
+      })
+
+      mainWindow.loadURL(
+        url.format({
+          pathname: path.join(__dirname, `/dist/faktury/index.html`),
+          protocol: "file:",
+          slashes: true
+        })
+      );
+      // Open the DevTools.
+      mainWindow.webContents.openDevTools()
+
+      mainWindow.on('closed', function () {
+        mainWindow = null
+      })
+    }
+
+    app.on('ready', createWindow)
+
+    app.on('window-all-closed', function () {
+      if (process.platform !== 'darwin') app.quit()
+    })
+
+    app.on('activate', function () {
+      if (mainWindow === null) createWindow()
+    })
